03dff4d03b710fcc99e7a54389e6bd9583f3b907
Jonathan M <jgit@tuta.io>
2025-12-22 00:51:49 -0800

    test: add some unit tests for the `pacman_hook` module

--- src/pacman_hook.rs
+++ src/pacman_hook.rs
@@ -92,3 +92,104 @@ pub fn generate_removal(bin_info: &WrappedBinaryInfo) -> String {
         unwrapped_path = bin_info.unwrapped_path.display(),
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    mod trim_path_root {
+        use super::*;
+
+        #[test]
+        fn test_absolute() {
+            let input = PathBuf::from("/home/user/file");
+            let expected = PathBuf::from("home/user/file");
+            assert_eq!(trim_path_root(input), expected);
+        }
+
+        #[test]
+        fn test_relative() {
+            let input = PathBuf::from("relative/path");
+            let expected = PathBuf::from("relative/path");
+            assert_eq!(trim_path_root(input), expected);
+        }
+    }
+
+    mod get_hook_path_tests {
+        use super::*;
+
+        fn test_get_hook_path_helper(binary_name: &str, action: Action, expected_suffix: &str) {
+            let expected_program_name = env!("CARGO_PKG_NAME");
+            let expected_path =
+                format!("{HOOK_DIR}/{binary_name}-{expected_program_name}-{expected_suffix}.hook");
+
+            let result = get_hook_path(binary_name, action);
+            assert_eq!(result.to_string_lossy(), expected_path);
+        }
+
+        #[test]
+        fn test_install_or_update() {
+            test_get_hook_path_helper("test_binary", Action::InstallOrUpdate, "install");
+        }
+
+        #[test]
+        fn test_removal() {
+            test_get_hook_path_helper("test_binary", Action::Removal, "remove");
+        }
+    }
+
+    #[test]
+    fn test_generate_install_and_update() {
+        let bin_info = WrappedBinaryInfo {
+            wrapped_path: PathBuf::from("/usr/bin/test_executable"),
+            wrapped_exec_name: "test_executable".to_string(),
+            unwrapped_path: PathBuf::from("/usr/bin/original_executable"),
+        };
+
+        let hook_script_path = PathBuf::from("/etc/test_script.sh");
+
+        let result = generate_install_and_update(&bin_info, &hook_script_path);
+
+        let expected = formatdoc! { r#"
+              [Trigger]
+              Type = File
+              Operation = Install
+              Operation = Upgrade
+              Target = usr/bin/test_executable
+
+              [Action]
+              Description = Wrapping test_executable executable...
+              When = PostTransaction
+              Exec = /etc/test_script.sh
+              "#
+        };
+
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_generate_removal() {
+        let bin_info = WrappedBinaryInfo {
+            wrapped_path: PathBuf::from("/usr/bin/wrapped_exec"),
+            wrapped_exec_name: "wrapped_exec".to_string(),
+            unwrapped_path: PathBuf::from("/usr/bin/original_exec"),
+        };
+
+        let result = generate_removal(&bin_info);
+
+        let expected = formatdoc! { r#"
+              [Trigger]
+              Type = File
+              Operation = Remove
+              Target = usr/bin/wrapped_exec
+
+              [Action]
+              Description = Removing traces of wrapper for wrapped_exec executable...
+              When = PostTransaction
+              Exec = /usr/bin/rm "/usr/bin/original_exec"
+              "#
+        };
+
+        assert_eq!(result, expected);
+    }
+}
